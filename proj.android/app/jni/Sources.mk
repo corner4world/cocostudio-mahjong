4216c79bda1f987703a223ee12d0b0c40d58d532
iqiang <iqiang@163.com>
2018-07-14 11:56:51 +0800

     清理文件

--- proj.android/app/jni/Sources.mk
+++ proj.android/app/jni/Sources.mk
@@ -1,12 +1,11 @@
 LOCAL_SRC_FILES := hellocpp/main.cpp \
 ../../../Classes/UI/BaseUILayer.cpp \
 ../../../Classes/UI/WelcomeDlg.cpp \
-../../../Classes/UI/SetLayer.cpp \
 ../../../Classes/UI/GameLayer.cpp \
+../../../Classes/UI/SettingDlg.cpp \
 ../../../Classes/UI/AlertDlg.cpp \
 ../../../Classes/App/AppDelegate.cpp \
 ../../../Classes/Manager/DialogManager.cpp \
-../../../Classes/Manager/GameSceneManager.cpp \
 ../../../Classes/Common/UIHelper.cpp \
 ../../../Classes/Common/GameConfig.cpp \
 ../../../Classes/GameLogic/AIPlayer.cpp \
